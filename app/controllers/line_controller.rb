451c0486d528dd5b709e90a87b240fb234170c24
Donald Chow <donald7290@gmail.com>
2024-03-02 22:48:27 +0900

    respond task with use message

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -2,12 +2,17 @@ class LineController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    p params
+    tasks = Todoists::GetTasksFromProject.call
+    message = BuildMessage::ListTasks.call(tasks:)
+    user_id = ENV['DC_LINE_ID']
+    Line::PushMessageJob.perform_later(user_id, [message])
+
+    render json: { status: 'ok' }, Status: :ok
   end
 
   private
 
   def webhook_params
-    params.permit()
+    params.permit
   end
 end
